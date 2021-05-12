bb823b3ab0956f1b7d5fb281fbac394f82663265
kkr970 <58022426+kkr970@users.noreply.github.com>
2021-05-12 22:07:43 +0900

    Unit ChaseAI

---
+++ DesertLeague/Assets/PrivateSpace/IckSu/Scripts/Unit.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit: MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+    public Transform target; // 네비게이션 추적 타겟
+
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    NavMeshAgent nav;
+    Animator anim;
+
+    bool isChase;
+    bool isAttack;
+    
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        ChaseStart();//따라가기를 실행함
+    }
+
+    void Update()
+    {
+        if(isChase)
+            nav.SetDestination(target.position); // target의 위치로 따라감
+    }
+
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
+
+    void FreezeVelocity()
+    {
+        if (isChase)
+        {
+            rigid.angularVelocity = Vector3.zero;
+            rigid.velocity = Vector3.zero;
+        }
+    }
+
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+}
--- DesertLeague/Assets/Scripts/Player.cs
+++ DesertLeague/Assets/Scripts/Player.cs
@@ -25,7 +25,6 @@ public class Player : MonoBehaviour
 
     void StopToWall()
     {
-        Debug.DrawRay(transform.position, transform.forward * 5, Color.green);
         isBorder = Physics.Raycast(transform.position, transform.forward, 2, LayerMask.GetMask("Wall"));
     }
 
